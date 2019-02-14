1d6582c5147bb609ee341a9441748ff4f8fb7e0a
fblundo <federico.blundo@gmail.com>
2019-02-14 22:23:14 +0000

    completed routing assignment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Link } from 'react-router-dom'; // importing the Route and Link 
 import './App.css';
 import Landing from './components/Landing'; //importing the component
 import Library from './components/Library'; //importing the component
-import Album from './components/Library'; //importing the component
 
 
 class App extends Component {
@@ -20,8 +19,7 @@ class App extends Component {
          <main>
           <Route exact path="/" component={Landing} />  //creating a route for each page with corresponding path; we specify "exact path" because we only want the Landing component to render when the path is /,
           <Route path="/library" component={Library} /> //creating a route for each page with corresponding path
-          <Route path="/album" component={Album} /> //creating a route for each page with corresponding path
-         </main>
+           </main>
       </div>
 
     );
