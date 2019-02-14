8e9406d23ff0e28d97834b64d31f7e66059a5228
fblundo <federico.blundo@gmail.com>
2019-02-14 22:20:41 +0000

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom'; // importing the Route and Link components to implement a basic router
 import './App.css';
+import Landing from './components/Landing'; //importing the component
+import Library from './components/Library'; //importing the component
+import Album from './components/Library'; //importing the component
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <header>
+          <nav>
+             <Link to='/'>Landing</Link>
+             <Link to='/library'>Library</Link>
+           </nav>
+           <h1>Bloc Jams</h1>
+         </header>
+         <main>
+          <Route exact path="/" component={Landing} />  //creating a route for each page with corresponding path; we specify "exact path" because we only want the Landing component to render when the path is /,
+          <Route path="/library" component={Library} /> //creating a route for each page with corresponding path
+          <Route path="/album" component={Album} /> //creating a route for each page with corresponding path
+         </main>
       </div>
+
     );
   }
 }
